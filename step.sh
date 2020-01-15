a268441de5e708954383f5dd126f28c92962ce2d
mrjorgexavier <mr.jorge.xavier@gmail.com>
2020-01-15 20:14:29 -0300

    Mudando proposta do step - temporario -

--- step.sh
+++ step.sh
@@ -1,47 +1,12 @@
 #!/bin/bash
-set -ex
+#set -ex
 
-function install_jq () {
-    apt-get update
-    apt-get install -y curl jq
-}
+INPUT='tv-production-release.apk=>https://app.bitrise.io/artifact/29147490/p/52e6ffff6ea33100359e6660cb674fd3|mobile-production-universal-release.apk=>https://app.bitrise.io/artifact/29147494/p/f3523ef744ce1d3d8607cbc2378cd34a|mobile-production-universal-release20200115193033.apk=>https://app.bitrise.io/artifact/29147499/p/8924996f1a0d6c1f96798cfb866f359e|mobile-production-debug-androidTest.apk=>https://app.bitrise.io/artifact/29147482/p/b37aa7957a82789e054b1fef8c8d67c5|mobile-production-debug-androidTest20200115190912.apk=>https://app.bitrise.io/artifact/29147483/p/4a3b998e35984a0bbf00e0ff34ac0052|mobile-production-debug.apk=>https://app.bitrise.io/artifact/29147484/p/9f6c6552835984e103a671e9104497c3|mobile-production-release.apk=>https://app.bitrise.io/artifact/29147487/p/030144fc3a80d597bef350adb1723a7c'
+COUNT=$(echo $INPUT| grep -o "=>" | wc -l)
 
-if ! [ -x "$(command -v jq)" ] 
-then
-    echo "jq json parser is not installed, installing..."
-    install_jq
-else
-    echo "jq json parser already installed, continuing..."
-fi
-
-# $GITHUB_TOKEN 
-# $GITHUB_BRANCH
-# $GITHUB_OWNER
-# $GITHUB_REPO
-# $GITHUB_ENVIRONMENT
-# $GITHUB_TARGET_URL
-
-
-
-requestHeader="Content-Type: application/json -H Authorization: token ${github_token}"
-
-#MARK: DEPLOY REQUEST
-createDeploymentRequestUrl="https://api.github.com/repos/${github_owner}/${github_repo}/deployments"
-
-createDeploymentData=$(echo '{"ref": '\"${github_branch}\"', "description": "Deploying", "payload": ""}')
-
-createDeploymentResult=$(curl -d '{"ref": '\"${github_branch}\"', "description": "Deploying", "payload": ""}' -H "Content-Type: application/json" -H "Authorization: token ${github_token}" -X POST $createDeploymentRequestUrl)
-
-deploymentId=$(echo $createDeploymentResult | jq '.id')
-
-echo "deployment request: $createDeploymentResult"
-
-
-#MARK: STATUS REQUEST
-createStatusRequestUrl="https://api.github.com/repos/${github_owner}/${github_repo}/deployments/$deploymentId/statuses"
-
-createStatusData=$(echo '{"environment": '\"${github_environment}\"',"state": "success", "target_url": '\"${github_target_url}\"', "description": "Deployment test finish"}')
-
-createStatusResult=$(curl -d $createStatusData -H $requestHeader -X POST $createStatusRequestUrl)
-
-echo "status request: $createStatusResult"
+for ((INDEX = 1 ; INDEX <= $COUNT ; INDEX++)); do
+    KEY=$(echo $(echo $INPUT| cut -d'|' -f $INDEX)| cut -d'=' -f 1)
+    VALUE=$(echo $(echo $INPUT| cut -d'|' -f $INDEX)| cut -d'=' -f 2)
+    VALUE=${VALUE#">"}
+    echo "$INDEX -> $KEY : $VALUE"
+done
