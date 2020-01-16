b173262e270ceed7f45e1418d599e407871420f7
mrjorgexavier <mr.jorge.xavier@gmail.com>
2020-01-16 15:49:07 -0300

    adicionando echo pra logar informação do script

--- step.sh
+++ step.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+echo "Input: $INPUT"
+echo "Target: $TARGET"
+
 COUNT=$(echo $INPUT| grep -o "=>" | wc -l)
+
+echo "Artifacts count: $COUNT "
 
 for ((INDEX = 1 ; INDEX <= $COUNT ; INDEX++)); do
     KEY=$(echo $(echo $INPUT| cut -d'|' -f $INDEX)| cut -d'=' -f 1)
     VALUE=$(echo $(echo $INPUT| cut -d'|' -f $INDEX)| cut -d'=' -f 2)
     VALUE=${VALUE#">"}    
-    if [ "$KEY" == "$TARGET" ]; then        
+    echo "Parse Artifact: $KEY -> $VALUE"
+    if [ "$KEY" == "$TARGET" ]; then    
         BITRISE_CUSTOM_ARTIFACT_URL=$VALUE
+        echo "Target Finded: $VALUE"
     fi
 done
+
+echo "Finded Artifact: ( $BITRISE_CUSTOM_ARTIFACT_URL )"
